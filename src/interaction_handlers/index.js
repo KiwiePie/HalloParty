d9a11ce882a81b54c0ff93b62d43dc0dc533ca20
ig1711 <cgrecent@gmail.com>
2022-10-19 13:49:02 +0600

    added message-create handler

--- src/interaction_handlers/index.js
+++ src/interaction_handlers/index.js
@@ -1,9 +1,11 @@
 import chatInputCommand from './chatInputCommand';
 import button from './button';
+import autocomplete from './autocomplete';
 
 /** @param { import('discord.js').BaseInteraction } interaction */
 export default async function (interaction) {
   if (interaction.isChatInputCommand())
     return await chatInputCommand(interaction);
   if (interaction.isButton()) return await button(interaction);
+  if (interaction.isAutocomplete()) return await autocomplete(interaction);
 }
