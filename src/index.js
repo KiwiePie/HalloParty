5209461fd707ba00e681e6e4077c6a592302ecfa
ig1711 <cgrecent@gmail.com>
2022-10-19 13:47:53 +0600

    Added message-create handler

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import { Client } from 'discord.js';
 import envMissingErr from '../utils/env_missing_err';
 import mongoose from 'mongoose';
 import interactionHandler from './interaction_handlers';
+import { messageCreate as messageCreateHandler } from './other_event_handlers';
 
 if (!process.env.TOKEN) envMissingErr('TOKEN');
 
-const client = new Client({ intents: [] });
+const client = new Client({ intents: 1 | (1 << 9) | (1 << 15) });
 
 console.log(
   'Wait for the database to get connected and the bot to get ready...'
@@ -19,6 +20,10 @@ client.on('interactionCreate', async interaction => {
   await interactionHandler(interaction);
 });
 
+client.on('messageCreate', messageCreateHandler);
+
+client.on('error', console.log);
+
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -27,5 +32,3 @@ mongoose
   .then(() => console.log('Database connected...'));
 
 client.login(process.env.TOKEN);
-
-process.on('unhandledRejection', rejection => console.log(rejection));
