4709d440c46435c2cf4b134493d255a7bba39384
ig1711 <cgrecent@gmail.com>
2022-10-22 00:22:31 +0600

    updated clean party command

--- src/commands/clean_party_channels.js
+++ src/commands/clean_party_channels.js
@@ -1,15 +1,17 @@
 import PartyManager from '../managers/party_manager';
 
-export const clean_party_cahnnels = {
+export const clean_party_channels = {
   data: {
     name: 'clean_party_channels',
-    description: 'Mod command for deleting a party and related channels',
+    description:
+      'Mod (with MANAGE_CHANNELS perm) command for deleting a party and related channels',
     options: [
       {
         type: 3,
         name: 'party',
         description: 'Choose the party to delete',
         autocomplete: true,
+        required: true,
       },
     ],
   },
@@ -17,6 +19,12 @@ export const clean_party_cahnnels = {
   /** @param { import('discord.js').ChatInputCommandInteraction } interaction */
   handle: async function (interaction) {
     const i = await interaction.deferReply({ ephemeral: true });
+
+    if (!interaction.member.permissions.has(16n))
+      return await i.interaction.editReply({
+        content:
+          'You dont have the permission. You need the MANAGE_CHANNELS permision.',
+      });
 
     const partyId = interaction.options.get('party').value;
     if (!partyId)
